8e76499e10fb10244e1142c77074a02cc1a06507
GustavoAlvarezP <g.alvarez0202@gmail.com>
2021-10-08 23:39:53 -0500

    Experiencia de practica 3 parte 2: busqueda de clientes

---
+++ BusquedaClientes/BusquedaClientes/WebForm1.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="BusquedaClientes.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <link href="StyleSheet1.css" rel="stylesheet" type="text/css" />
+</head>
+<body class="nuevoEstilo5">
+    <form id="form1" runat="server" class="nuevoEstilo4">
+        <div>
+        </div>
+        <asp:Label ID="Label2" runat="server" CssClass="nuevoEstilo11" Height="157px" Text="Búsqueda de CLIENTES" Width="527px"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Código" Width="70px"></asp:Label>
+        <asp:TextBox ID="txtCodigo" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnBuscar" runat="server" OnClick="btnBuscar_Click" Text="Buscar" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnLimpiar" runat="server" OnClick="btnLimpiar_Click" Text="Limpiar" />
+        <br />
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Nombres" Width="70px"></asp:Label>
+        <asp:TextBox ID="txtNombres" runat="server" ReadOnly="True" Width="320px"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Dirección" Width="70px"></asp:Label>
+        <asp:TextBox ID="txtDireccion" runat="server" ReadOnly="True" Width="320px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label5" runat="server" Text="Teléfono" Width="70px"></asp:Label>
+        <asp:TextBox ID="txtTelefono" runat="server" ReadOnly="True"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label6" runat="server" Text="E-Mail" Width="70px"></asp:Label>
+        <asp:TextBox ID="txtMail" runat="server" ReadOnly="True" Width="320px"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label7" runat="server" Text="Edad" Width="70px"></asp:Label>
+        <asp:TextBox ID="txtEdad" runat="server" ReadOnly="True" Width="50px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btnMostrar" runat="server" OnClick="btnMostrar_Click" Text="Mostrar Todos los Clientes" />
+        <br />
+        <br />
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <br />
+        <asp:Label ID="lblResult" runat="server"></asp:Label>
+        <br />
+    </form>
+    <p class="nuevoEstilo4">
+&nbsp;</p>
+</body>
+</html>
---
+++ BusquedaClientes/BusquedaClientes/WebForm1.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+namespace BusquedaClientes
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string connectionString = "Data Source = LAPTOP-LO8K6NU9\\SQLEXPRESS;" +
+            "Initial Catalog = VENTAS; integrated Security=SSPI";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnBuscar_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "";
+            string selectSQL = "select * from clientes where numero =" + txtCodigo.Text;
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(selectSQL, con);
+            SqlDataReader dr;
+            try
+            {
+                con.Open();
+                dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    txtNombres.Text = dr[1].ToString();
+                    txtDireccion.Text = dr["direccion"].ToString();
+                    txtTelefono.Text = dr[3].ToString();
+                    txtMail.Text = dr[4].ToString();
+                    txtEdad.Text = dr[5].ToString();
+                }
+                con.Close();
+                dr.Close();
+            }
+            catch (Exception err)
+            {
+                lblResult.Text = "Error al registrar cliente";
+                lblResult.Text += err.Message;
+            }
+        }
+
+        protected void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtCodigo.Text = "";
+            txtNombres.Text = "";
+            txtDireccion.Text = "";
+            txtTelefono.Text = "";
+            txtMail.Text = "";
+            txtEdad.Text = "";
+        }
+
+        protected void btnMostrar_Click(object sender, EventArgs e)
+        {
+            string selectSQL = "Select * from clientes";
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(selectSQL, con);
+            SqlDataReader dr;
+            try
+            {
+                con.Open();
+                dr = cmd.ExecuteReader();
+                GridView1.DataSource = dr;
+                GridView1.DataBind();
+                con.Close();
+            }
+            catch (Exception err)
+            {
+                lblResult.Text = "Error al registrar cliente";
+                lblResult.Text += err.Message;
+            }
+        }
+    }
+}
